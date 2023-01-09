4fa5261e91fa354f63f0dcec186e742f6724e45b
DESKTOP-HJH6RV4\Admin <swarupa.madha@honeywell.com>
2023-01-09 16:12:56 +0530

    first sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/TestComp';
+
+//function WelcomeMessage(){
+  //const message = 'Welcome React JS'
+  //return <h1><p>Welcome React JS</p></h1>
+//}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hello/>
     </div>
+
   );
 }
 
---
+++ src/components/TestComp.tsx
@@ -0,0 +1,3 @@
+import React from "react";
+const Hello= () => <h1>testing app</h1>
+export default Hello;
